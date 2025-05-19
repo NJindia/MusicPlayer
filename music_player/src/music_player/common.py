ee75e59092ef3d282fcd8c949e3efcd187cb563c
Nikhil Jindia <nikhiljindia@gmail.com>
2025-05-18 23:51:02 -0400

    Start lib header

--- music_player/src/music_player/common.py
+++ music_player/src/music_player/common.py
@@ -5,36 +5,9 @@ from functools import cache
 from pathlib import Path
 
 import dacite
-from PySide6.QtCore import QEvent, Qt, Signal
-from PySide6.QtWidgets import QLabel, QWidget
 from dacite import Config, from_dict
 
 from music_player.music_importer import get_music_df, Music
-
-
-class HoverableUnderlineLabel(QLabel):
-    clicked = Signal(QEvent)
-
-    def __init__(self, text: str, parent: QWidget | None = None):
-        super().__init__(parent)
-        self.setTextFormat(Qt.TextFormat.RichText)
-        self.setTextInteractionFlags(Qt.TextInteractionFlag.TextBrowserInteraction)
-        self.setAttribute(Qt.WidgetAttribute.WA_Hover)
-        self.setMouseTracking(True)
-        self.setText(text)
-
-    def leaveEvent(self, event: QEvent | None) -> None:
-        f = self.font()
-        f.setUnderline(False)
-        self.setFont(f)
-
-    def enterEvent(self, event: QEvent | None) -> None:
-        f = self.font()
-        f.setUnderline(True)
-        self.setFont(f)
-
-    def mousePressEvent(self, event: QEvent | None) -> None:
-        self.clicked.emit(event)
 
 
 @dataclass
